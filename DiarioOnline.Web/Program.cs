f7fd7531db34437bec5accf75685e8f92bc1644b
Dravvos <daniddias53@gmail.com>
2024-11-03 19:24:54 -0300

    ajustes referencias css e js

--- DiarioOnline.Web/Program.cs
+++ DiarioOnline.Web/Program.cs
@@ -11,18 +11,20 @@ builder.Services.AddDbContext<DiarioContext>(options =>
 {
     options.UseSqlServer(Environment.GetEnvironmentVariable("DiarioConnection"));
 });
+
 builder.Services.AddSession();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<CustomMiddleware>();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseMiddleware<CustomMiddleware>();
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
